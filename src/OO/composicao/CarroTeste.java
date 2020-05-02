0e7aec1e962196f9ec95d295279adbb9f22fba4f
Rodrigo Prado <rodrigo.ribeiroprado@gmail.com>
2020-05-02 15:11:41 -0300

    Object Oriented

---
+++ src/OO/composicao/CarroTeste.java
@@ -0,0 +1,34 @@
+package OO.composicao;
+
+public class CarroTeste {
+	
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro ();
+		
+		System.out.println(c1.estaLigado());
+		
+		c1.ligar();
+		System.out.println(c1.estaLigado());
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.acelear();
+		c1.acelear();
+		c1.acelear();
+		c1.acelear();
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		c1.frear();
+		System.out.println(c1.motor.giros());
+		
+		//Relação bidirecional
+		System.out.println(c1.motor.carro.motor.carro.motor.giros());
+		
+	}
+
+}
